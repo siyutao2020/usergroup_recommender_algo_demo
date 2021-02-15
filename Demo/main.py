7d5df22d6cd5693cd85ac95bbb07e20186a7152b
SiyuTAOhome <siyutao2020@u.northwestern.edu>
2021-02-14 21:28:24 -0500

    File batch commit

---
+++ Demo/main.py
@@ -0,0 +1,12 @@
+import math
+import os
+import torch
+import torch.distributions.constraints as constraints
+import pyro
+from pyro.optim import Adam
+from pyro.infer import SVI, Trace_ELBO, config_enumerate, TraceEnum_ELBO, infer_discrete
+import pyro.distributions as dist
+
+import matplotlib.pyplot as plt
+
+torch.linalg.norm()
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Algorithm for Recommender System Based on User Grouping Demo
+
 This repo contains demo code for a recommender system based on user grouping. The user grouping is learned through a generative machine learning model trained with stochastic variational inference.
